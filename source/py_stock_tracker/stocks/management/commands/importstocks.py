fc42df75365e886bd340a2a06f24ab5331c82afe
Matt Larsen <utegrad@gmail.com>
2019-02-10 19:02:07 -0700

    test unique constraints on stock model. start command modules

---
+++ source/py_stock_tracker/stocks/management/commands/importstocks.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from stocks.models import Stock
+
+
+class Command(BaseCommand):
+    help = 'Import stocks from csv file in import_data folder'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', nargs='+', type=str)
--- source/py_stock_tracker/stocks/tests.py
+++ source/py_stock_tracker/stocks/tests.py
@@ -1,5 +1,27 @@
+import unittest
+
+from django.db import IntegrityError
 from django.test import TestCase
 
 from .models import Stock
 
-# Create your tests here.
+
+class StockTestCases(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_stock_symbols_are_unique(self):
+        stock_name_1 = 'Stock 1'
+        stock_name_2 = 'Stock 2'
+        stock_symbol = 'blah'
+        Stock.objects.create(name=stock_name_1, symbol=stock_symbol)
+        with self.assertRaisesRegexp(IntegrityError, 'UNIQUE constraint failed'):
+            Stock.objects.create(name=stock_name_2, symbol=stock_symbol)
+
+    def test_stock_name_are_unique(self):
+        stock_name = 'Stock'
+        stock_symbol_1 = 'STK'
+        stock_symbol_2 = 'STOK'
+        Stock.objects.create(name=stock_name, symbol=stock_symbol_1)
+        with self.assertRaisesRegexp(IntegrityError, 'UNIQUE constraint failed'):
+            Stock.objects.create(name=stock_name, symbol=stock_symbol_2)
